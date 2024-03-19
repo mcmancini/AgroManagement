4ef2de0dc4dfcd4b6943910d600250578db10071
mcmancini <m.c.mancini@exeter.ac.uk>
2024-03-19 13:11:30 +0000

    Updated init of core package with AWS session for downloading Sentinel data

--- agromanagement/core/__init__.py
+++ agromanagement/core/__init__.py
@@ -3,8 +3,10 @@
 # Mattia Mancini (m.c.mancini@exeter.ac.uk), December 2023
 # ========================================================
 """
-agromanagement package initialisation file
+agromanagement core package initialisation file
 """
+import boto3
+import rasterio
 
 from agromanagement.core.config_parser import ConfigReader
 from agromanagement.utility.paths import ROOT_DIR
@@ -13,3 +15,5 @@ config_path = ROOT_DIR + "\\config.ini"
 # pylint: disable=E1101
 app_config = ConfigReader(config_path)
 # pylint: enable=E1101
+
+aws_session = rasterio.session.AWSSession(boto3.Session(), requester_pays=True)
