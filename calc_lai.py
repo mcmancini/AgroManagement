f25f53e6e4d2e67eeec49e358f9430942a3e310a
mcmancini <m.c.mancini@exeter.ac.uk>
2024-05-22 10:23:36 +0100

    Added sample script to compute LAI for a parcel and a timeframe of interest.

--- calc_lai.py
+++ calc_lai.py
@@ -1,28 +1,46 @@
-from agromanagement.utility.lai_fusion import LAI_S1S2_fusion
+# -*- coding: utf-8 -*-
+# Copyright (c) 2024 LEEP, University of Exeter (UK)
+# Mattia Mancini (m.c.mancini@exeter.ac.uk), May 2024
+# ===================================================
+"""
+Sample script loading a parcel and computing LAI based on weather data
+downloaded with download_weather.py and the LAI generator class in
+utils/lai_generator.py. More info in both scripts.
+"""
+import geopandas as gpd
+
+from agromanagement.utility.lai_generator import LaiGenerator
 from agromanagement.utility.paths import ROOT_DIR
 
-PARCEL_PATH = "D:/Documents/GitHub/AgroManagement/resources/lcm2021_tile_11_1014_647.geojson"
+PARCELS_PATH = (
+    "D:/Documents/GitHub/AgroManagement/resources/lcm2021_tile_11_1014_647.geojson"
+)
+parcels = gpd.read_file(PARCELS_PATH)
+parcel = parcels.iloc[1025:1026, :]
+parcel_name = parcel["gid"].iloc[0]
+parcel_path = f"D:/Documents/GitHub/AgroManagement/resources/{parcel_name}.geojson"
+parcel.to_file(parcel_path, driver="GeoJSON")
 
-SENTINEL_1_UNZIPPED_FOLDER = "D:/Documents/Data/Sentinel/Sentinel_1/Raw/"
-SENTINEL_2_UNZIPPED_FOLDER = "D:/Documents/Data/Sentinel/Sentinel_2/Raw/"
+SENTINEL_1_UNZIPPED_FOLDER = "D:/Documents/Data/Sentinel/Sentinel_1/"
+SENTINEL_2_UNZIPPED_FOLDER = "D:/Documents/Data/Sentinel/Sentinel_2/"
 ERA_DIR = "resources/era_5/"
 
 START_DATE = "2019-04-01"
 END_DATE = "2019-04-30"
 
-lai = LAI_S1S2_fusion(
-    jsonloc=PARCEL_PATH,
-    FNAME="prova",
+lai = LaiGenerator(
+    jsonloc=parcel_path,
+    filename="prova",
     workingdir=ROOT_DIR,
-    s1dir=SENTINEL_1_UNZIPPED_FOLDER,
-    s2dir=SENTINEL_2_UNZIPPED_FOLDER,
-    ECMWF_ERA5_VPD=ERA_DIR,
+    s1_dir=SENTINEL_1_UNZIPPED_FOLDER,
+    s2_dir=SENTINEL_2_UNZIPPED_FOLDER,
+    era_dir=ERA_DIR,
     snap_graphs_dir="C:/Users/mcm216/.snap/graphs/",
     snap_gtp_dir="C:/Program Files/esa-snap/bin/",
     startdate=START_DATE,
-    enddate=END_DATE
+    enddate=END_DATE,
 )
 
-# lai.S1_to_VVVH()
-lai.S2_to_LAI()
+lai.s1_to_vvvh()
+lai.s2_to_lai()
 lai.lai_ts_creation()
